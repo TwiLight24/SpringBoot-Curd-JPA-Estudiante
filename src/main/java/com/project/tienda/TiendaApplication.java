84a2e8f9151e5505926cc01a82ff01c7226c62da
TwiLight24 <106403635+TwiLight24@users.noreply.github.com>
2025-02-16 11:51:13 -0500

    Avance m. 39:10

--- src/main/java/com/project/tienda/TiendaApplication.java
+++ src/main/java/com/project/tienda/TiendaApplication.java
@@ -1,30 +1,31 @@
 package com.project.tienda;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.project.tienda.entidad.Estudiante;
-import com.project.tienda.repositorio.EstudianteRepositorio;
+
+
 
 @SpringBootApplication
-public class TiendaApplication implements CommandLineRunner{
+public class TiendaApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaApplication.class, args);
 	}
 
-	@Autowired
-	private EstudianteRepositorio repositorio;
+	
 
-	@Override
-	public void run(String... args) throws Exception {
+
+	//@Override
+//	public void run(String... args) throws Exception {
+		/* 
 		Estudiante estudiante1 = new Estudiante("Sebastian", "Quispe", "se_edu@hotmail.com");
 		repositorio.save(estudiante1);
 
 		Estudiante estudiante2 = new Estudiante("Daniela", "Meneses", "daniela@hotmail.com");
 		repositorio.save(estudiante2);
-	}
+		*/
+	//}
 
 }
